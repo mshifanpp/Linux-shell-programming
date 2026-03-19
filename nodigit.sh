be87fe0450a2ce6b22d8c6982974410ea8d0d2bb
mshifanpp <128384863+mshifanpp@users.noreply.github.com>
2026-03-19 12:42:01 +0530

    completed

--- nodigit.sh
+++ nodigit.sh
@@ -1,25 +1,37 @@
+#!/bin/bash
 
 if [ $# -ne 1 ]
 then
-echo "syntax <$0>< 3digit number>"
-exit 1
+    echo "syntax: <$0> <number>"
+    exit 1
 fi
-num=$1
-o=`expr  $num % 10`
-t=`expr $num / 10 % 10` 
-h=`expr  $num / 100 ` 
-for i in $h $t $o 
+n=$1
+rev=0
+c=0
+while [ $n -ne 0 ]
 do
-case $i in
-1) echo -n  "one  " ;;
-2) echo -n  "two " ;;
-3) echo  -n "three " ;;
-4) echo -n  "four " ;;
-5) echo  -n "five " ;;
-6) echo -n  "six " ;;
-7) echo -n  "seven " ;;
-8) echo -n  "eight " ;;
-9) echo -n  "nine " ;;
-0) echo -n  "zero " ;;
-esac
+    digit=`expr $n % 10`             
+    rev=`expr $rev \* 10 + $digit`    
+    c=`expr $c + 1`                   
+    n=`expr $n / 10`                 
 done
+while [ $c -ne 0 ]
+do
+    digit=`expr $rev % 10`
+
+    case $digit in
+    1) echo -n "one " ;;
+    2) echo -n "two " ;;
+    3) echo -n "three " ;;
+    4) echo -n "four " ;;
+    5) echo -n "five " ;;
+    6) echo -n "six " ;;
+    7) echo -n "seven " ;;
+    8) echo -n "eight " ;;
+    9) echo -n "nine " ;;
+    0) echo -n "zero " ;;
+    esac
+    rev=`expr $rev / 10`
+    c=`expr $c - 1`
+done
+echo
