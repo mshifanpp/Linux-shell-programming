d137b8f76712a5d670d6b6a66148ea9728443d67
mshifanpp <128384863+mshifanpp@users.noreply.github.com>
2026-04-23 12:50:53 +0530

    completd

---
+++ linuxword.sh
@@ -0,0 +1,17 @@
+if [ $# -eq 0 ]
+then
+echo "syntax is<$0><filename>"
+exit 1
+fi
+if [ ! -f $1 ]
+then
+echo "file doesn't exist"
+exit 1
+fi
+echo "------original file --"
+cat $1
+grep -v -I "linux" $1 > temp.txt
+mv temp.txt $1
+echo "----updating file affter removing linux ---"
+cat $1
+ 
